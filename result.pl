64c72916897498dc121cc8685c868f010a3459cc
Ralph Schuler <mail@ralphschuler.ch>
2020-12-18 17:23:34 +0100

    beautify code a bit

--- result.pl
+++ result.pl
@@ -14,29 +14,29 @@ my @players;
 my %ranking;
 
 my $stmt = qq(SELECT DISTINCT(white) FROM results;);
-my $sth = $dbh->prepare( $stmt );
-my $rv = $sth->execute() or die $DBI::errstr;
+my $sth  = $dbh->prepare( $stmt );
+my $rv   = $sth->execute() or die $DBI::errstr;
 
-while(my $row = $sth->fetchrow_array()) {
+while (my $row = $sth->fetchrow_array()) {
       push(@players, $row);
 }
 
 for my $p (@players) {
     my $sum;    
     my $stmt = qq(select result from results where white = '$p';);
-    my $sth = $dbh->prepare( $stmt );
-    my $rv = $sth->execute() or die $DBI::errstr;
+    my $sth  = $dbh->prepare( $stmt );
+    my $rv   = $sth->execute() or die $DBI::errstr;
 
-    while(my $row = $sth->fetchrow_array()) {
+    while (my $row = $sth->fetchrow_array()) {
           $sum++ if $row eq '1-0';
           $sum = $sum + 0.5 if $row eq '0.5-0.5';
     }
     
     $stmt = qq(select result from results where black = '$p';);
-    $sth = $dbh->prepare( $stmt );
-    $rv = $sth->execute() or die $DBI::errstr;
+    $sth  = $dbh->prepare( $stmt );
+    $rv   = $sth->execute() or die $DBI::errstr;
 
-    while(my $row = $sth->fetchrow_array()) {
+    while (my $row = $sth->fetchrow_array()) {
           $sum++ if $row eq '0-1';
           $sum = $sum + 0.5 if $row eq '0.5-0.5';
           $cnt++;
@@ -44,7 +44,6 @@ for my $p (@players) {
     
     $ranking{$p} = $sum;
 }
-
 
 foreach my $name (reverse sort { $ranking{$a} <=> $ranking{$b} or $a cmp $b } keys %ranking) {
     printf "%-20s %s\n", $name, $ranking{$name};
@@ -54,4 +53,3 @@ print "------------------------\n";
 print "sum                  $cnt\n";
 
 $dbh->disconnect();
-
