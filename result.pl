124f370ce26fb51169911850502ae2d9d72c2d01
Ralph Schuler <mail@ralphschuler.ch>
2020-12-18 16:01:01 +0100

    result script and db added

---
+++ result.pl
@@ -0,0 +1,55 @@
+use strict;
+use warnings;
+use DBI;
+use Data::Dumper;
+
+my $driver   = "SQLite";
+my $database = "tournament.sqlite";
+my $dsn      = "DBI:$driver:dbname=$database";
+my $userid   = "";
+my $password = "";
+my $dbh = DBI->connect($dsn, $userid, $password, { RaiseError => 1 }) or die $DBI::errstr;
+my $cnt;
+my @players;
+my %ranking;
+
+my $stmt = qq(SELECT DISTINCT(white) FROM results;);
+my $sth = $dbh->prepare( $stmt );
+my $rv = $sth->execute() or die $DBI::errstr;
+
+while(my $row = $sth->fetchrow_array()) {
+      push(@players, $row);
+}
+
+for my $p (@players) {
+    my $sum;    
+    my $stmt = qq(select result from results where white = '$p';);
+    my $sth = $dbh->prepare( $stmt );
+    my $rv = $sth->execute() or die $DBI::errstr;
+
+    while(my $row = $sth->fetchrow_array()) {
+          $sum++ if $row eq '1-0';
+          $sum = $sum + 0.5 if $row eq '0.5-0.5';
+    }
+    
+    $stmt = qq(select result from results where black = '$p';);
+    $sth = $dbh->prepare( $stmt );
+    $rv = $sth->execute() or die $DBI::errstr;
+
+    while(my $row = $sth->fetchrow_array()) {
+          $sum++ if $row eq '0-1';
+          $sum = $sum + 0.5 if $row eq '0.5-0.5';
+          $cnt++;
+    }    
+    
+    $ranking{$sum} = $p;
+}
+
+for my $s (reverse sort keys %ranking) {
+    print "$s\t$ranking{$s}\n";
+}
+
+print "--------\n";
+print "$cnt\n";
+
+$dbh->disconnect();
