ca971741091e23532c81df1680d87f05fd698e5f
Ralph Schuler <mail@ralphschuler.ch>
2020-12-28 11:19:16 +0100

    Ability to edit results via command line

---
+++ edit_results.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+
+use DBI;
+use strict;
+use Term::Choose qw(choose);
+
+my $driver   = "SQLite";
+my $database = "tournament.sqlite";
+my $dsn      = "DBI:$driver:dbname=$database";
+my $userid   = "";
+my $password = "";
+my $dbh      = DBI->connect($dsn, $userid, $password, { RaiseError => 1 }) or die $DBI::errstr;
+my $gameno = $ARGV[0];
+
+print "Spielnummer: " unless defined $gameno;
+chomp ($gameno = <>);
+
+updategame($gameno);
+
+sub updategame {
+    
+    my $gameno = shift;
+    my $white;
+    my $black;
+
+    my $stmt = qq(SELECT id, no, game, white, black, result from results WHERE no = $gameno;);
+    my $sth  = $dbh->prepare( $stmt );
+    my $rv   = $sth->execute() or die $DBI::errstr;
+
+    if($rv < 0) {
+        print $DBI::errstr;
+    }
+
+    while(my @row = $sth->fetchrow_array()) {
+        print "ID     = ". $row[0] . "\n";
+        print "NO     = ". $row[1] . "\n";
+        print "GAME   = ". $row[2] . "\n";
+        print "WHITE  = ". $row[3] . "\n";
+        print "BLACK  = ". $row[4] . "\n";
+        print "RESULT = ". $row[5] . "\n\n";
+        
+        $white = $row[3];
+        $black = $row[4];        
+        
+    }
+    
+    my $result = choose([$white, $black, 'remis'], { prompt => 'Wer hat gewonnen?' }) or exit;    
+    my $stmt;
+    
+    if ($result eq $white) {
+        print "$white hat gewonnen (weiss)\n";
+        $stmt = qq(UPDATE results set result = '1-0' WHERE no = $gameno;);
+    } elsif ($result eq $black) {
+        print "$black hat gewonnen (schwarz)\n";
+        $stmt = qq(UPDATE results set result = '0-1' WHERE no = $gameno;);
+    } else {
+        print "remis\n";
+        $stmt = qq(UPDATE results set result = 'remis' WHERE no = $gameno;);
+    }
+
+    my $sth  = $dbh->prepare( $stmt );
+    my $rv   = $sth->execute() or die $DBI::errstr;
+
+    $dbh->disconnect();
+    
+}
